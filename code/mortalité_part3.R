e5ddc2aeaec2405afb817ddbfe87b6124939df37
adelodemo <a.janiaud@hotmail.fr>
2023-11-20 14:56:33 +0100

    update DC19

--- code/mortalité_part3.R
+++ code/mortalité_part3.R
@@ -7,13 +7,15 @@ library(questionr)
 #On importe les données d'état civil et de recensement
 load("/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/DC19.rdata")
 load("/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/RP2020.rdata")
-FD_INDREGZE_2019 <- read.csv("~/Desktop/ADA-Rapport-Dept-13/data/FD_INDREGZE_2019.csv", sep=";")
+load("/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/RP19_13.rdata")
 
 #On filtre pour avoir seulement le département 13
 DC19$DEPDEC <- as.numeric(as.character(DC19$DEPDEC))
 DC19_13 <- filter(DC19,DEPDOM==13)
 
+save(DC19_13,file ="/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/DC19_13.rdata")
+
 RP19_13 <- filter(FD_INDREGZE_2019,DEPT==13)
-save("/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/RP19_13.rdata")
+save(RP19_13,file = "/Users/adelejnd/Desktop/ADA-Rapport-Dept-13/data/RP19_13.rdata")
 
 #On veut calculer des taux de mortalité 
